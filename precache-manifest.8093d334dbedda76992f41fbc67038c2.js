12dd9a6939902a26aa45639d22d712001c8cb00c
Namita123 <namitadubey4@gmail.com>
2022-02-08 11:45:51 +0530

    Updates

---
+++ precache-manifest.8093d334dbedda76992f41fbc67038c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c32c29630e157d6e58fc3e72442ee75b",
+    "url": "/monsters-rolodex-complete/index.html"
+  },
+  {
+    "revision": "753d81dbff328984d3c1",
+    "url": "/monsters-rolodex-complete/static/css/main.6f686940.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex-complete/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex-complete/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "753d81dbff328984d3c1",
+    "url": "/monsters-rolodex-complete/static/js/main.f0459bc4.chunk.js"
+  },
+  {
+    "revision": "4b601965c2149392e1f2",
+    "url": "/monsters-rolodex-complete/static/js/runtime-main.b3c2bfd0.js"
+  }
+]);
